37d40a84fb5d19a91aa312c5abd3942f66f6ad57
mk <mkoraichi@outlook.com>
2017-12-23 00:15:34 +0000

    2nd commit l Client

--- Client.java
+++ Client.java
@@ -17,20 +17,7 @@ public class Client {
     
     public static void main(String[] args) throws UnknownHostException, IOException {
    
-        System.out.println("----------Client--------");
-        String msg,arreteMessage="bye";
-        Scanner scaner=new Scanner(System.in);
-        Tunel tunel=new Tunel("127.0.0.1",1234);
-        do{
-            System.out.println("attend!!");
-            msg=tunel.readChat();
-            System.out.println(msg);
-            System.out.print("vous : ");
-            msg=scaner.nextLine();
-            tunel.sendChat(msg);
-            
-        }while(!msg.equals(arreteMessage));
-        
+        System.out.println("Hello GIT");
     
     
     }
